e5063696accfb6baa2b47679f16a8ec55962d24c
Aurelie Perichon <aperichon@eugeniaschool.com>
2026-05-31 00:44:19 +0200

    Fine tune home character vertical position

--- app/page.tsx
+++ app/page.tsx
@@ -131,7 +131,7 @@ export default function HomePage() {
                     src={assetPath("/images/home/personnage-interrogatif.png")}
                       alt=""
                       aria-hidden="true"
-                      className="block w-44 max-w-none origin-[23%_82%] -translate-x-[24%] -translate-y-[65%] sm:w-64 sm:-translate-x-[49%] sm:-translate-y-[61%]"
+                      className="block w-44 max-w-none origin-[23%_82%] -translate-x-[24%] -translate-y-[64%] sm:w-64 sm:-translate-x-[49%] sm:-translate-y-[60%]"
                     />
                 </span>
                 <span className="relative z-10">{t("home.cta")}</span>
